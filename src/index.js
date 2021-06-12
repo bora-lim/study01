8340d81065acb2aa21daae4d3a95ab75d8c696a4
bora-lim <yksysr@gmail.com>
2021-06-12 10:01:21 +0900

    react setting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import React from 'react'
+import ReactDom from 'react-dom'
+import HomePage from './pages/home'
+import EditPage from './pages/edit'
+
+ReactDom.render(<EditPage />, document.getElementById('app'))
+
+
+
+
+// commonjs (es5) : require -> 옛날방법, node 명령어 사용가능
+// esm (es6) : import -> 최신방법, 트랜스파일링, 번들링 필요
+// so, 기본설정 파일들은 보통 번들링, 트랜스파일링을 거치지 않기때문에 require 방식 사용!
